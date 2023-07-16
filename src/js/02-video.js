68cfc58bdda26211f730a69e78f46932d3429ab2
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-16 15:22:24 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,13 @@
-import player from '@vimeo/player';
+import Player from '@vimeo/player';
+console.log(Player);
+import throttle from 'lodash.throttle';
 
-const KEY_STORAGE = 'videoplayer-current-time';
-localStorage.setItem(KEY_STORAGE, 'Hello');
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframeEl);
+
+// const KEY_STORAGE = 'videoplayer-current-time';
+// localStorage.setItem(KEY_STORAGE, 'Hello');
 // player.setCurrentTime(30.456);
-
 
 // const iframe = document.querySelector('iframe');
 //     const player = new Vimeo.Player(iframe);
@@ -15,3 +19,11 @@ localStorage.setItem(KEY_STORAGE, 'Hello');
 //     player.getVideoTitle().then(function(title) {
 //         console.log('title:', title);
 //     });
+
+// player.on('play', function(data) {
+//     playing
+//     // data is an object containing properties specific to that event
+// });
+// var callback = function() {timeupdate};
+
+// player.off('eventName', callback);
