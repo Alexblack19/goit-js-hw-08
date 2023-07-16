d1ab7be3e3ae994e203884f38275ac746a5418de
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-16 17:17:49 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,29 +6,12 @@ const KEY_STORAGE = 'videoplayer-current-time';
 const vimeoPlayer = new Player(iframeEl);
 
 vimeoPlayer.on('timeupdate', throttle(onCurrentTime, 1000));
+function onCurrentTime(e) {
+  localStorage.setItem(KEY_STORAGE, e.seconds);
+}
 
-function onCurrentTime (e)
+const savedTime = localStorage.getItem(KEY_STORAGE);
+console.log(savedTime);
+vimeoPlayer.setCurrentTime(savedTime || 0);
 
-// vimeoPlayer.on('play', function () {
-//   console.log('played the video!');
-// });
-
-// vimeoPlayer.off('play', function () {
-//   console.log('stop the video!');
-// });
-
-// vimeoPlayer.pause().then(function () {
-//   console.log('the video was paused');
-// });
-
-//     player.getVideoTitle().then(function(title) {
-//         console.log('title:', title);
-//     });
-
-// player.on('play', function(data) {
-//     playing
-//     // data is an object containing properties specific to that event
-// });
-// var callback = function() {timeupdate};
-
-// player.off('eventName', callback);
+vimeoPlayer.off('play', onCurrentTime);
