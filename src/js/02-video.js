00ca5269cb3d2c1fcb23a18e1f2c4c8fb1fa0753
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-16 14:19:53 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import player from '@vimeo/player';
 
+const KEY_STORAGE = 'videoplayer-current-time';
+localStorage.setItem(KEY_STORAGE, 'Hello');
+// player.setCurrentTime(30.456);
+
+
+// const iframe = document.querySelector('iframe');
+//     const player = new Vimeo.Player(iframe);
+
+//     player.on('play', function() {
+//         console.log('played the video!');
+//     });
+
+//     player.getVideoTitle().then(function(title) {
+//         console.log('title:', title);
+//     });
